59c2c16459377c09b8238f9efe36a0700fd14514
Aayush Prajapati <aayush.prajapati0430@gmail.com>
2024-11-01 12:51:51 +0545

    link icon meta tag

--- src/app/aayush/page.tsx
+++ src/app/aayush/page.tsx
@@ -4,13 +4,21 @@ import { Card } from "@/index";
 const page = () => {
   return (
     <div>
-      <div className="ml-[200px] mt-10">
+      <div className="ml-10 mt-10 flex bg-red-200">
         <Card
           product={{
             name: "Watch",
             price: "1200",
             image:
               "https://www.titannepal.com/cdn/shop/files/2648WM04.jpg?v=1716471937",
+          }}
+        />
+        <Card
+          product={{
+            name: "Attack Shark Mouse",
+            price: "2800",
+            image:
+              "https://i5.walmartimages.com/asr/8de78d17-0283-49b3-a074-00720ca6ad0c.5b875553625c30a4416ad0545562a447.jpeg",
           }}
         />
       </div>
